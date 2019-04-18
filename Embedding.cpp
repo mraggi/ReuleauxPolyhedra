1b6a55ab014ad1cc074e31bafc43503b1194410b
Miguel Raggi <mraggi@gmail.com>
2019-04-17 23:24:07 -0500

    Added physics stuff. Doesn't seem to work very well...

--- Embedding.cpp
+++ Embedding.cpp
@@ -12,13 +12,15 @@ using Population = std::vector<Individual>;
 struct DiffEvoParams
 {
     int population_size{40};
-    int num_epochs{10000000};
+    int num_epochs{5000000};
     double force{0.5};
-    double change_prob{0.4};
+    double change_prob{0.5};
 };
 
 struct UnitDistanceGraph
 {
+    using vectors = std::vector<Point3d>;
+
     UnitDistanceGraph(const Graph& G) : A(G.num_vertices(), Row(G.num_vertices(), 0))
     {
         for (auto u : G.vertices())
@@ -57,20 +59,117 @@ struct UnitDistanceGraph
         return total;
     }
 
+    void PhysicsMutator(Individual& p, int num_epochs) const
+    {
+        int N = p.size();
+
+        vectors a(N);
+        vectors v(N);
+
+        for (int i = 0; i < num_epochs; ++i)
+        {
+            calculate_forces(p, a);
+            simulate_time(p, v, a);
+        }
+    }
+
+    void calculate_forces(const vectors& p, vectors& a) const
+    {
+        int n = A.size();
+
+        constexpr double K = 1.; // spring constant
+
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                Point3d P = p[i];
+                Point3d Q = p[j];
+                Point3d D = Q - P;
+                double d = distance(P, Q);
+                D /= d;
+                if (A[i][j])
+                {
+                    double x = d - 1.;
+                    Point3d force = K*x*D;
+                    a[i] += force;
+                    a[j] -= force;
+                }
+                else
+                {
+                    double x = d - 0.5;
+                    Point3d force = K*x*D/10.;
+                    a[i] += force;
+                    a[j] -= force;
+                }
+            }
+        }
+    }
+
+    void simulate_time(vectors& p, vectors& v, vectors& a) const
+    {
+        constexpr double time_step = 1e-3;
+        int n = p.size();
+        for (int i = 0; i < n; ++i)
+        {
+            v[i] += a[i]*time_step;
+            v[i] *= 0.99999;
+            p[i] += v[i]*time_step;
+        }
+        for (auto& aa : a)
+            aa = Point3d(0., 0., 0.);
+    }
+
     Matrix A;
+};
+
+void clamp(Point3d& p, double r)
+{
+    if (p.x < -r)
+        p.x = -r;
+    else if (p.x > r)
+        p.x = r;
+
+    if (p.y < -r)
+        p.y = -r;
+    else if (p.y > r)
+        p.y = r;
+
+    if (p.z < -r)
+        p.z = -r;
+    else if (p.z > r)
+        p.z = r;
+}
+
+struct Normalize
+{
+    Normalize(const UnitDistanceGraph& u, Random& r) : U(u), R(r) {}
+    void operator()(Individual& I)
+    {
+        //         if (R.random_real(0.,1.) < 0.01)
+        //         {
+        //             U.PhysicsMutator(I,1000);
+        //         }
+
+        for (auto& p : I)
+        {
+            clamp(p, 4.);
+        }
+    }
+
+    const UnitDistanceGraph& U;
+    Random& R;
 };
 
 std::vector<Point3d> FindUnitDistEmbedding(const Graph& G, const DiffEvoParams& P = DiffEvoParams())
 {
     int n = G.num_vertices();
+    UnitDistanceGraph U(G);
 
     auto population = Population(P.population_size);
     std::generate(population.begin(), population.end(), [n]() {
         return generate_random_points(n, 2.);
     });
-    std::cout << population[0] << std::endl;
-
-    UnitDistanceGraph U(G);
 
     DifferentialEvolver D(population, U);
 
@@ -78,46 +177,62 @@ std::vector<Point3d> FindUnitDistEmbedding(const Graph& G, const DiffEvoParams& 
 
     auto pbar = tq::trange(P.num_epochs);
 
-    int how_many_to_add = P.population_size*0.5;
+    int how_many_to_add = 2*P.population_size;
     int add_every_nth = P.num_epochs/how_many_to_add;
+
+    Normalize normalizer(U, R);
 
     for (int epoch : pbar)
     {
         double prob = R.random_real(P.change_prob/2, P.change_prob*1.5);
         double force = R.random_real(P.force/2, P.force*1.5);
-        D.step(prob, force);
+
+        D.step(prob, force, normalizer);
+
         pbar << "Best: " << D.best_cost << " at epoch " << epoch
              << " with a population of: " << D.population_size();
 
-        if (D.best_cost < 1.e-10)
-            break;
+        if (D.best_cost < 1.e-14)
+            return D.best;
 
         if ((epoch + 1)%add_every_nth == 0)
         {
-            D.insert_individual(generate_random_points(n, 2.));
+            auto W = generate_random_points(n, 2.);
+            if (R.random_real(0.,1.) < 0.5)
+                U.PhysicsMutator(W, 1000);
+            D.insert_individual(W);
         }
     }
 
+    std::cout << "# WARNING: failed to find good embedding" << std::endl;
     return D.best;
 }
 
 int main(int argc, char* argv[])
 {
-
     std::vector<std::string> args(argv, argv + argc);
 
-    if (argc != 2)
+    if (argc < 2)
     {
         std::cout << "Usage: " << args[0] << " <FILE>" << std::endl;
         return 1;
     }
 
     auto ALL = ParseFile(args[1]);
-    int i = 0;
 
-    for (GraphAndUnitDistanceGraph& GFU : ALL)
+    int start = 0;
+    int end = ALL.size();
+
+    if (argc == 4)
     {
-        std::cout << "# Original Graph" << std::endl;
+        start = std::stoi(args[2]);
+        end = std::stoi(args[3]);
+    }
+
+    for (int i = start; i < end; ++i)
+    {
+        GraphAndUnitDistanceGraph& GFU = ALL[i];
+        std::cout << "# Original Graph " << i << " in [" << start << ", " << end << ")" << std::endl;
         std::cout << GFU.G << std::endl;
 
         std::cout << "# Unit Distance Graph" << std::endl;
@@ -130,9 +245,7 @@ int main(int argc, char* argv[])
         std::cout << "# Embedding (with cost " << cost << ")\n";
         std::cout << embedding << std::endl;
 
-        ++i;
-        std::cerr << "\nDone: " << i << " of " << ALL.size() << std::endl;
-        //         break;
+        std::cerr << "\nDone: " << i << " in range [" << start << ", " << end << ")" << std::endl;
     }
 
     return 0;
--- create_embeddings.sh
+++ create_embeddings.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-./build/embedding Data/data04.txt > Data/embeddings04.txt
-./build/embedding Data/data05.txt > Data/embeddings05.txt
-./build/embedding Data/data06.txt > Data/embeddings06.txt
-./build/embedding Data/data07.txt > Data/embeddings07.txt
-./build/embedding Data/data08.txt > Data/embeddings08.txt
-./build/embedding Data/data09.txt > Data/embeddings09.txt
-./build/embedding Data/data10.txt > Data/embeddings10.txt
-#./build/embedding Data/data11.txt > Data/embeddings11.txt
-#./build/embedding Data/data12.txt > Data/embeddings12.txt
-#./build/embedding Data/data13.txt > Data/embeddings13.txt
-#./build/embedding Data/data14.txt > Data/embeddings14.txt
+#./build/embedding Data/data04.txt > Data/embeddings04.txt
+#./build/embedding Data/data05.txt > Data/embeddings05.txt
+#./build/embedding Data/data06.txt > Data/embeddings06.txt
+#./build/embedding Data/data07.txt > Data/embeddings07.txt
+#./build/embedding Data/data08.txt > Data/embeddings08.txt
+#./build/embedding Data/data09.txt > Data/embeddings09.txt
+#./build/embedding Data/data10.txt > Data/embeddings10.txt
+
+# De 11 son 24
+# ./build/embedding Data/data11.txt > Data/embeddings11.txt 
+
+
+# De 12 son 72
+# ./build/embedding Data/data12.txt > Data/embeddings12_0.txt 0 36 &
+# ./build/embedding Data/data12.txt > Data/embeddings12_1.txt 36 72 &
+
+
+# De 13 son 212
+# ./build/embedding Data/data13.txt > Data/embeddings13_0.txt 0 50 &
+# ./build/embedding Data/data13.txt > Data/embeddings13_1.txt 50 100 &
+# ./build/embedding Data/data13.txt > Data/embeddings13_2.txt 100 150 &
+# ./build/embedding Data/data13.txt > Data/embeddings13_3.txt 150 200 &
+# ./build/embedding Data/data13.txt > Data/embeddings13_4.txt 200 212 
+
+# De 14 son 674
+./build/embedding Data/data14.txt > Data/embeddings14_0.txt 0 50 &
+./build/embedding Data/data14.txt > Data/embeddings14_1.txt 50 100 &
+./build/embedding Data/data14.txt > Data/embeddings14_2.txt 100 150 &
+./build/embedding Data/data14.txt > Data/embeddings14_3.txt 150 200 &
+./build/embedding Data/data14.txt > Data/embeddings14_4.txt 200 250 &
+./build/embedding Data/data14.txt > Data/embeddings14_5.txt 250 300 &
+./build/embedding Data/data14.txt > Data/embeddings14_6.txt 300 350 &
+./build/embedding Data/data14.txt > Data/embeddings14_7.txt 350 400 &
+./build/embedding Data/data14.txt > Data/embeddings14_8.txt 400 450 &
+./build/embedding Data/data14.txt > Data/embeddings14_9.txt 450 500 &
+./build/embedding Data/data14.txt > Data/embeddings14_10.txt 500 550 &
+./build/embedding Data/data14.txt > Data/embeddings14_11.txt 550 600 &
+./build/embedding Data/data14.txt > Data/embeddings14_12.txt 600 650 &
+./build/embedding Data/data14.txt > Data/embeddings14_13.txt 650 674 
