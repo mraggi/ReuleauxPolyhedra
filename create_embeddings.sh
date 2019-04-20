9bb8b0b01bb914a088119f294fc268db4aee58e2
Miguel Raggi <mraggi@gmail.com>
2019-04-19 21:37:32 -0500

    Well, I should go back on this one at some point, but made it look not only for acceptable embeddings, but for the 'best ones'

--- create_embeddings.sh
+++ create_embeddings.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#./build/embedding Data/data04.txt > Data/embeddings04.txt
-#./build/embedding Data/data05.txt > Data/embeddings05.txt
-#./build/embedding Data/data06.txt > Data/embeddings06.txt
-#./build/embedding Data/data07.txt > Data/embeddings07.txt
-#./build/embedding Data/data08.txt > Data/embeddings08.txt
-#./build/embedding Data/data09.txt > Data/embeddings09.txt
-#./build/embedding Data/data10.txt > Data/embeddings10.txt
+./build/embedding Data/data04.txt > Data/embeddings04.txt
+./build/embedding Data/data05.txt > Data/embeddings05.txt
+./build/embedding Data/data06.txt > Data/embeddings06.txt
+./build/embedding Data/data07.txt > Data/embeddings07.txt
+./build/embedding Data/data08.txt > Data/embeddings08.txt
+./build/embedding Data/data09.txt > Data/embeddings09.txt
+./build/embedding Data/data10.txt > Data/embeddings10.txt
 
 # De 11 son 24
 #./build/embedding Data/data11.txt > Data/embeddings11.txt &
@@ -25,25 +25,25 @@
 #./build/embedding Data/data13.txt > Data/embeddings13_4.txt 200 212 & 
 
 # De 14 son 674
-./build/embedding Data/data14.txt > Data/aembeddings14_0.txt 0 25 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_0.txt 0 25 &
 #./build/embedding Data/data14.txt > Data/aembeddings14_1.txt 25 50 &
 #./build/embedding Data/data14.txt > Data/aembeddings14_2.txt 50 75 &
 #./build/embedding Data/data14.txt > Data/aembeddings14_3.txt 75 100 &
 #./build/embedding Data/data14.txt > Data/aembeddings14_4.txt 100 125 &
-./build/embedding Data/data14.txt > Data/aembeddings14_5.txt 125 150 &
-./build/embedding Data/data14.txt > Data/aembeddings14_6.txt 150 175 &
-./build/embedding Data/data14.txt > Data/aembeddings14_7.txt 175 200 &
-./build/embedding Data/data14.txt > Data/aembeddings14_8.txt 200 225 &
-./build/embedding Data/data14.txt > Data/aembeddings14_9.txt 225 250 &
-./build/embedding Data/data14.txt > Data/aembeddings14_10.txt 250 275 &
-./build/embedding Data/data14.txt > Data/aembeddings14_11.txt 275 300 &
-./build/embedding Data/data14.txt > Data/aembeddings14_12.txt 300 325 &
-./build/embedding Data/data14.txt > Data/aembeddings14_13.txt 325 350 &
-./build/embedding Data/data14.txt > Data/aembeddings14_14.txt 350 375 &
-./build/embedding Data/data14.txt > Data/aembeddings14_15.txt 375 400 &
-./build/embedding Data/data14.txt > Data/aembeddings14_16.txt 400 425 &
-./build/embedding Data/data14.txt > Data/aembeddings14_17.txt 425 450 &
-./build/embedding Data/data14.txt > Data/aembeddings14_18.txt 450 475 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_5.txt 125 150 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_6.txt 150 175 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_7.txt 175 200 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_8.txt 200 225 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_9.txt 225 250 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_10.txt 250 275 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_11.txt 275 300 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_12.txt 300 325 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_13.txt 325 350 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_14.txt 350 375 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_15.txt 375 400 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_16.txt 400 425 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_17.txt 425 450 &
+#./build/embedding Data/data14.txt > Data/aembeddings14_18.txt 450 475 &
 ./build/embedding Data/data14.txt > Data/aembeddings14_19.txt 475 500 &
 ./build/embedding Data/data14.txt > Data/aembeddings14_20.txt 500 525 &
 ./build/embedding Data/data14.txt > Data/aembeddings14_21.txt 525 550 &
